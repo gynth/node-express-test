c365a73b60fa43d2418c5487d111594ce4a6cbcf
gynth <gynth90@gmail.com>
2021-03-04 14:49:11 +0900

    1

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
 var mysql = require('mysql');
 const db = mysql.createPool({
+    connectionLimit: 50,
+    //기본10초 connectTimeout 
     host : '211.231.136.182',
     user : 'root',
     password : 'dk',
     database : 'local'
-    
 });
 
 module.exports = db;
